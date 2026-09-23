f568925fb542088aa70e3ab53c3fca373cabe1c1
Aidan Casimir <142954872+aidancasimir@users.noreply.github.com>
2026-09-23 13:05:03 -0500

    Add SDK with large files using Git LFS

---
+++ DW3_QM33_SDK_1/SDK/Firmware/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca9d77db03c68963f763c250d0672556ad522f3ff4fbe3301b0ff3a855aefe1a
+size 831
---
+++ DW3_QM33_SDK_1/SDK/Tools/GUI/Linux/QorvoUwbExplorer-1.0.0-final-x86_64-install.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6f1136cb8601aa3c004f242a16c16510f04b68f7b0cb8fe007672d7b569228b5
+size 138124984
